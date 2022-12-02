45a91b20c56d48f890c777dff0b41dc05937c859
Hardiyanto <kapsulasi@gmail.com>
2022-12-02 12:50:25 +0700

    FInal version 1

--- lib/main.dart
+++ lib/main.dart
@@ -30,12 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  /* 
-    Silahkan Rubah API Starting point di sini
-    Layanan API harus bebas CORS
-    Layanan API harus dapat diakses oleh chrome dan native aplikasi
-    Layanan API content type nya application/json
-  */
 
   //final String apiStartPoint = 'https://api.npoint.io/153137cab500f8c8f7bd';
   final String apiStartPoint = 'https://web.hardiyanto.my.id/jsons/profile.json';
